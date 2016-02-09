505ba04e93fa1f315a793cf547fc3f4981a56821
Nothing-Dev <alfredomartinez_1423@hotmail.com>
2016-02-09 19:11:16 -0430

    Import postrecoveryboot.sh from TWRP royss repo
    Signed-off-by: Nothing-Dev <alfredomartinez_1423@hotmail.com>

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,2 +1,11 @@
 #!/sbin/sh
-dd if=/dev/zero of=/dev/block/mmcblk0p13 bs=512 count=8192 > /dev/null 2>&1 ; sync
+
+# The partition to use is named "PARAM" and can be found by dumping the PIT.
+
+# 0: charge
+# 1: direct
+# 2: recovery_enter
+# 3: recovery_end
+# 4: download_end
+
+echo -n -e '\x03\x00\x00\x00' > /dev/block/platform/msm_sdcc.3/by-num/p14
