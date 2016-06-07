4cfbe8e257f4355ccbaae65c17923c0dcf909eed
lesleyxxx12 <lesleyvanderlee318@gmail.com>
2016-06-07 06:13:42 +0200

    Add hudson script
    Change-Id: Ifd4606bfec4cd6badaa73b025b64a172d0a46f10

---
+++ build-hudson.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+termdir="vendor/cm/proprietary"
+
+termf="$termdir/Term.apk"
+libarmt="$termdir/lib/armeabi/libjackpal-androidterm*.so"
+libarme="$termdir/lib/armeabi/libjackpal-termexec*.so"
+libmipst="$termdir/lib/mips/libjackpal-androidterm*.so"
+libmipse="$termdir/lib/mips/libjackpal-termexec*.so"
+libx86t="$termdir/lib/x86/libjackpal-androidterm*.so"
+libx86e="$termdir/lib/x86/libjackpal-termexec*.so"
+
+if ! ( [ -f "$termf" ] && [ -e $libarmt ] && [ -e $libarme ] &&
+       [ -e $libmipst ] && [ -e $libmipse ] && [ -e $libx86t ] && [ -e $libx86e ] )
+then
+	./vendor/cm/get-prebuilts > /dev/null 2>&1
+        if ! [ $? -eq 0 ]; then
+    		exit
+	fi
+fi
+
+chmod +x build/envsetup.sh
+
+source build/envsetup.sh > /dev/null 2>&1
+if ! [ $? -eq 0 ]; then
+    exit
+fi
+
+brunch cm_royss-userdebug
